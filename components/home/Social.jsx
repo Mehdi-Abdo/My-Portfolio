34dc6d99d1c0afc3353b889de7126eed463f358b
Mehdi <mehdiabdelaoui0@gmail.com>
2025-06-10 20:57:51 +0100

    addd project on github

---
+++ components/home/Social.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/mehdi_abdo.48/" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/mehdi-abdelaoui" className="home__social-icon" target="_blank">
+            <i className="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/Mehdi-Abdo" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
---
+++ components/work/Data.jsx
@@ -0,0 +1,48 @@
+import Work1 from '../../src/assets/Work1.png';
+import Work2 from '../../src/assets/Work2.png';
+import Work3 from '../../src/assets/Work3.png';
+
+
+export const projectsData = [
+	{
+		id: 1,
+		image: "",
+		title: 'Weather ',
+		category: 'app',
+		demoUrl: 'https://tic-tac-toe-project-x-o.vercel.app/',
+		sourceUrl: 'https://github.com/soumyajiitt/tic-tac-toe',
+		status: 'completed',
+	},
+
+	{
+		id: 2,
+		image: Work2,
+		title: 'RepoQuest',
+		category: 'app',
+		demoUrl: 'https://repoquest.vercel.app/',
+		sourceUrl: 'https://github.com/soumyajiitt/RepoQuest',
+		status: 'completed',
+	},
+	{
+		id: 3,
+		image: Work3,
+		title: 'Portfolio Website',
+		category: 'web',
+		demoUrl: 'https://soumyajitbasak.netlify.app/',
+		sourceUrl: 'https://github.com/soumyajiitt/Portfolio',
+		status: 'completed',
+	},
+	
+];
+
+export const projectsNav = [
+	{
+		name: 'all',
+	},
+	{
+		name: 'app',
+	},
+	{
+		name: 'web',
+	},
+];
