9df75eee35cbdb19794e1d807c01fd64fd0b4730
Mehdi <mehdiabdelaoui0@gmail.com>
2025-06-16 14:43:59 +0100

    add new link about prayer times

--- components/work/Data.jsx
+++ components/work/Data.jsx
@@ -1,5 +1,6 @@
 import Work1 from '../../src/assets/portfolio.png'
 import Work2 from '../../src/assets/weather.png'
+import Work3 from '../../src/assets/prayer.png'
 
 
 export const projectsData = [
@@ -7,7 +8,7 @@ export const projectsData = [
 		id: Work1,
 		image: Work1,
 		title: 'My Porfolio ',
-		category: 'app',
+		category: 'web',
 		demoUrl: 'https://myportfolio2-eta.vercel.app/',
 		sourceUrl: 'https://github.com/Mehdi-Abdo/My-Portfolio.git',
 		status: 'completed',
@@ -16,9 +17,18 @@ export const projectsData = [
 		id: Work2,
 		image: Work2,
 		title: 'Weather Dasboard ',
-		category: 'Web',
+		category: 'web',
 		demoUrl: 'https://weater-dashboard.netlify.app/',
 		sourceUrl: 'https://github.com/Mehdi-Abdo/Bolt-Weather.git',
+		status: 'completed',
+	},
+	{
+		id: Work3,
+		image: Work3,
+		title: 'Prayer Times ',
+		category: 'web',
+		demoUrl: 'https://praayer-times.netlify.app/',
+		sourceUrl: 'https://github.com/Mehdi-Abdo/Times-Prayer.git',
 		status: 'completed',
 	},
 
