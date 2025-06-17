8c59790e4f139b9d0960ee70b0a7795966794b9e
Mehdi <mehdiabdelaoui0@gmail.com>
2025-06-17 15:18:36 +0100

    add two work , first about movies , second about calculator

--- components/work/Data.jsx
+++ components/work/Data.jsx
@@ -1,6 +1,8 @@
 import Work1 from '../../src/assets/portfolio.png'
 import Work2 from '../../src/assets/weather.png'
 import Work3 from '../../src/assets/prayer.png'
+import Work4 from '../../src/assets/movies.png'
+import Work5 from '../../src/assets/calculator.png'
 
 
 export const projectsData = [
@@ -29,6 +31,24 @@ export const projectsData = [
 		category: 'web',
 		demoUrl: 'https://praayer-times.netlify.app/',
 		sourceUrl: 'https://github.com/Mehdi-Abdo/Times-Prayer.git',
+		status: 'completed',
+	},
+	{
+		id: Work4,
+		image: Work4,
+		title: 'Movies Dasboard',
+		category: 'web',
+		demoUrl: 'https://dashboard-movies.netlify.app/',
+		sourceUrl: 'https://github.com/Mehdi-Abdo/Learn-React-In-One-Project.git',
+		status: 'completed',
+	},
+	{
+		id: Work5,
+		image: Work5,
+		title: 'Calculator App',
+		category: 'app',
+		demoUrl: 'https://calculatrice-application.netlify.app/',
+		sourceUrl: 'https://github.com/Mehdi-Abdo/Calculator.git',
 		status: 'completed',
 	},
 
